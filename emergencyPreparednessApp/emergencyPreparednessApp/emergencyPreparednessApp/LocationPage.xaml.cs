1df10596b5fa06d49fee8ac5f95b424ba8a84a5f
jwitt <jwitt1234585@gmail.com>
2020-02-11 14:02:25 -0600

    some screens created

---
+++ emergencyPreparednessApp/emergencyPreparednessApp/emergencyPreparednessApp/LocationPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace emergencyPreparednessApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LocationPage : ContentPage
+    {
+        public LocationPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void RegionButton_OnClicked(object sender, EventArgs e)
+        {
+            change.Text = ((Button)sender).Text;
+            await Navigation.PushAsync(new MainRiskMapPage());
+        }
+
+    }
+}
---
+++ emergencyPreparednessApp/emergencyPreparednessApp/emergencyPreparednessApp/MainRiskMapPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace emergencyPreparednessApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainRiskMapPage : ContentPage
+    {
+        public MainRiskMapPage()
+        {
+            InitializeComponent();
+        }
+        int count = 0;
+        void RegionButton_OnClicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
+    }
+}
