65c454dfbcf5eb93ba6f5865a67808d5afc2e74e
jwitt <jwitt1234585@gmail.com>
2020-02-19 10:55:25 -0600

    actually commited to code files this time

--- emergencyPreparednessApp/emergencyPreparednessApp/emergencyPreparednessApp/LocationPage.xaml.cs
+++ emergencyPreparednessApp/emergencyPreparednessApp/emergencyPreparednessApp/LocationPage.xaml.cs
@@ -14,19 +14,59 @@ namespace emergencyPreparednessApp
     {
         private string popupName = "You sure?";
         private string popupText = "Choosing a location will allow you to get more specific information";
+        private string yesButton = "Yes";
+        private string noButton = "No";
         public LocationPage()
         {
             InitializeComponent();
         }
 
+        private async void LanguageButton_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+
         protected override void OnAppearing()
         {
             base.OnAppearing();
-            change.Text = App.Lang;
+            switch (App.Lang) {
+                case "e":
+                    break;
+                case "s":
+                    label1.Text = "Spanish What region are you from?";
+                    notSureButton.Text = "Not sure but in spanish";
+                    popupName = "You sure? But in spanish";
+                    popupText = "Spanish version of Choosing a location will allow you to get more specific information";
+                    yesButton = "Spanish yes";
+                    noButton = "Spanish no";
+                    changeLang.Text = "Spanish Change Language";
+                    break;
+                case "f":
+                    label1.Text = "French What region are you from?";
+                    notSureButton.Text = "Not sure but in french";
+                    popupName = "You sure? But in french";
+                    popupText = "French version of Choosing a location will allow you to get more specific information";
+                    yesButton = "French yes";
+                    noButton = "French no";
+                    changeLang.Text = "French Change Language";
+                    break;
+                case "g":
+                    label1.Text = "German What region are you from?";
+                    notSureButton.Text = "Not sure but in German";
+                    popupName = "You sure? But in german";
+                    popupText = "German version of Choosing a location will allow you to get more specific information";
+                    yesButton = "German yes";
+                    noButton = "German no";
+                    changeLang.Text = "German Change Language";
+                    break;
+                default:
+                    label1.Text = "Something broke";
+                    break;
+            }
         }
 
         private async void NotSureRegionButton_OnClicked(object sender, EventArgs e) {
-            bool answer = await DisplayAlert(popupName, popupText, "Yes", "No");
+            bool answer = await DisplayAlert(popupName, popupText, yesButton, noButton);
             if (answer) {
                 await Navigation.PushAsync(new MainRiskMapPage());
             } 
--- emergencyPreparednessApp/emergencyPreparednessApp/emergencyPreparednessApp/MainRiskMapPage.xaml.cs
+++ emergencyPreparednessApp/emergencyPreparednessApp/emergencyPreparednessApp/MainRiskMapPage.xaml.cs
@@ -16,11 +16,165 @@ namespace emergencyPreparednessApp
         {
             InitializeComponent();
         }
-        int count = 0;
-        void RegionButton_OnClicked(object sender, System.EventArgs e)
+
+        private async void RegionButton_OnClicked(object sender, EventArgs e)
         {
-            count++;
-            ((Button)sender).Text = $"You clicked {count} times.";
+            await Navigation.PushAsync(new LocationPage());
+        }
+
+        private async void LanguageButton_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            switch (App.Lang)
+            {
+                case "e":
+                    break;
+                case "s":
+                    label1.Text = "Spanish label1";
+                    landslide.Text = "Spanish landslide";
+                    flood.Text = "Spanish flood";
+                    fire.Text = "Spanish fire";
+                    fallenObject.Text = "Spanish fallenObject";
+                    label2.Text = "Spanish label2";
+                    tropicalStorm.Text = "Spanish tropicalStorm";
+                    earthquake.Text = "Spanish earthquake";
+                    volcanicActivity.Text = "Spanish volcanicActivity";
+                    changeLang.Text = "Spanish changeLang";
+                    changeLoc.Text = "Spanish changeLoc";
+                    break;
+                case "f":
+                    label1.Text = "French label1";
+                    landslide.Text = "French landslide";
+                    flood.Text = "French flood";
+                    fire.Text = "French fire";
+                    fallenObject.Text = "French fallenObject";
+                    label2.Text = "French label2";
+                    tropicalStorm.Text = "French tropicalStorm";
+                    earthquake.Text = "French earthquake";
+                    volcanicActivity.Text = "French volcanicActivity";
+                    changeLang.Text = "French changeLang";
+                    changeLoc.Text = "French changeLoc";
+                    break;
+                case "g":
+                    label1.Text = "German label1";
+                    landslide.Text = "German landslide";
+                    flood.Text = "German flood";
+                    fire.Text = "German fire";
+                    fallenObject.Text = "German fallenObject";
+                    label2.Text = "German label2";
+                    tropicalStorm.Text = "German tropicalStorm";
+                    earthquake.Text = "German earthquake";
+                    volcanicActivity.Text = "German volcanicActivity";
+                    changeLang.Text = "German changeLang";
+                    changeLoc.Text = "German changeLoc";
+                    break;
+                default:
+                    label1.Text = "Something broke";
+                    break;
+            }
+        }
+
+        public class ZoomImage : Image
+        {
+            private const double MIN_SCALE = 1;
+            private const double MAX_SCALE = 4;
+            private const double OVERSHOOT = 0.15;
+            private double StartScale;
+            private double LastX, LastY;
+
+            public ZoomImage()
+            {
+                var pinch = new PinchGestureRecognizer();
+                pinch.PinchUpdated += OnPinchUpdated;
+                GestureRecognizers.Add(pinch);
+
+                var pan = new PanGestureRecognizer();
+                pan.PanUpdated += OnPanUpdated;
+                GestureRecognizers.Add(pan);
+
+                var tap = new TapGestureRecognizer { NumberOfTapsRequired = 2 };
+                tap.Tapped += OnTapped;
+                GestureRecognizers.Add(tap);
+
+                Scale = MIN_SCALE;
+                TranslationX = TranslationY = 0;
+                AnchorX = AnchorY = 0;
+            }
+
+            protected override SizeRequest OnMeasure(double widthConstraint, double heightConstraint)
+            {
+                Scale = MIN_SCALE;
+                TranslationX = TranslationY = 0;
+                AnchorX = AnchorY = 0;
+                return base.OnMeasure(widthConstraint, heightConstraint);
+            }
+
+            private void OnTapped(object sender, EventArgs e)
+            {
+                if (Scale > MIN_SCALE)
+                {
+                    this.ScaleTo(MIN_SCALE, 250, Easing.CubicInOut);
+                    this.TranslateTo(0, 0, 250, Easing.CubicInOut);
+                }
+                else
+                {
+                    AnchorX = AnchorY = 0.5; //TODO tapped position
+                    this.ScaleTo(MAX_SCALE, 250, Easing.CubicInOut);
+                }
+            }
+
+            private void OnPanUpdated(object sender, PanUpdatedEventArgs e)
+            {
+                if (Scale > MIN_SCALE)
+                    switch (e.StatusType)
+                    {
+                        case GestureStatus.Started:
+                            LastX = TranslationX;
+                            LastY = TranslationY;
+                            break;
+                        case GestureStatus.Running:
+                            TranslationX = Clamp(LastX + e.TotalX * Scale, -Width / 2, Width / 2);
+                            TranslationY = Clamp(LastY + e.TotalY * Scale, -Height / 2, Height / 2);
+                            break;
+                    }
+            }
+
+            private void OnPinchUpdated(object sender, PinchGestureUpdatedEventArgs e)
+            {
+                switch (e.Status)
+                {
+                    case GestureStatus.Started:
+                        StartScale = Scale;
+                        AnchorX = e.ScaleOrigin.X;
+                        AnchorY = e.ScaleOrigin.Y;
+                        break;
+                    case GestureStatus.Running:
+                        double current = Scale + (e.Scale - 1) * StartScale;
+                        Scale = Clamp(current, MIN_SCALE * (1 - OVERSHOOT), MAX_SCALE * (1 + OVERSHOOT));
+                        break;
+                    case GestureStatus.Completed:
+                        if (Scale > MAX_SCALE)
+                            this.ScaleTo(MAX_SCALE, 250, Easing.SpringOut);
+                        else if (Scale < MIN_SCALE)
+                            this.ScaleTo(MIN_SCALE, 250, Easing.SpringOut);
+                        break;
+                }
+            }
+
+            private T Clamp<T>(T value, T minimum, T maximum) where T : IComparable
+            {
+                if (value.CompareTo(minimum) < 0)
+                    return minimum;
+                else if (value.CompareTo(maximum) > 0)
+                    return maximum;
+                else
+                    return value;
+            }
         }
     }
 }
